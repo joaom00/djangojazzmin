68ec41601121ac18bcaa5a31471c85043764ef3c
João Pedro Magalhães <joaom00.dev@gmail.com>
2024-01-28 12:16:13 -0300

    middleware

--- djangojazzmin/settings.py
+++ djangojazzmin/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
-    # "whitenoise.middleware.WhiteNoiseMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "djangojazzmin.urls"
